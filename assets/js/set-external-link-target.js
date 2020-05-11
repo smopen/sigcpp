99736b87c8b6a6a6f3f506cc2a5f65d6975cf4f6
Sean Murthy <murthys@wcsu.edu>
2020-05-10 20:08:19 -0400

    Change script to function callable on doc load

--- assets/js/set-external-link-target.js
+++ assets/js/set-external-link-target.js
@@ -5,15 +5,17 @@
 * Change the target attribute of all external links to "_blank"
 * - exclude links that already have a target (to allow customization in source)
 *
-* Include this script after html body so that all links are examined (at once) 
+* Call function setExternalLinkTarget at document onload 
 *
 * Core solution from: https://stackoverflow.com/a/4425214
 */
 
-var links = document.links;
+function setExternalLinkTarget() {
+    var links = document.links;
 
-for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-   if (links[i].hostname != window.location.hostname && links[i].target.length == 0) {
-       links[i].target = '_blank';
-   } 
+    for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+        if (links[i].hostname != window.location.hostname && links[i].target.length == 0) {
+            links[i].target = '_blank';
+        } 
+    }
 }
