c6a36a18fdd637feca24672992686782220d77e9
Sean Murthy <s.murthy@outlook.com>
2020-05-26 21:42:47 -0400

    Simplify logic to show external links

--- assets/js/set-external-link-target.js
+++ assets/js/set-external-link-target.js
@@ -1,33 +1,27 @@
----
-layout: blank
----
-
 /*
 * set-external-link-target.js
 * Sean Murthy
 *
-* Change the target attribute of external links to "_blank" and add a title and image
-* to inform the user that links open in a new tab
+* Change the target attribute of external links to "_blank" and add a title and a small
+* NE arrow to let the user know that links open in a new tab
 * - include only links attached to "a" element
 * - exclude links that already have a target (to allow customization in source)
 *
-* Function setExternalLinkTarget is expected to be called at document onload 
+* Assumes an appropriate style is set for the span element inserted
 *
-* Core solution of identifying external links from: https://stackoverflow.com/a/4425214
+* Function setExternalLinkTarget is expected to be called on document load 
 */
 
 function setExternalLinkTarget() {
-    var new_window_image_path = "{{ '/assets/images/external.png' | relative_url }}";
-    var new_window_image_html = '<img class="external" src="' + new_window_image_path + '"/>';
-    var links = document.links;
+    var anchors = document.getElementsByTagName("A");
     var host_name = window.location.hostname;
     
-    for (var i = 0, linksLength = links.length; i < linksLength; i++) {
-        var link = links[i];
-        if (link.tagName == 'A' && link.hostname != host_name && link.target.length == 0) {
-            link.target = '_blank';
-            link.title = 'Opens in new tab';
-            link.innerHTML += " <sup>&nearr;</sup>";
+    for (var i = 0, anchorsLength = anchors.length; i < anchorsLength; i++) {
+        var anchor = anchors[i];
+        if (anchor.hostname != host_name && anchor.target.length == 0) {
+            anchor.target = '_blank';
+            anchor.title = 'Opens in new tab';
+            anchor.insertAdjacentHTML('afterend', '<span class="external">&nearr;</span>');
        }
     }
 }
